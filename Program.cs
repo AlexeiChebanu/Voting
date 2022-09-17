dd35fa6d565f7d48d508ef176225a6bd50a82108
user3107 <0alexchebanu@gmail.com>
2022-09-17 18:10:36 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ListCollection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Topic topic = new Topic();
+        Console.WriteLine(topic.topic("Today`s voting topic is" + " "));
+
+        Voting voting = new Voting();
+        voting.dec();
+    }
+}
---
+++ Voting.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCollection
+{
+    class Topic
+    {
+        public string? topic(string message)
+        {
+            Console.Write("Election of ");
+            string top = Console.ReadLine();
+            return message + top;
+        }
+    }
+    class Voting
+    {
+        
+        public void dec()
+        {
+            Console.WriteLine("Make a decision: Yes or No\n0.Exit");
+            List<string> yesOrNo = new List<string>() { };
+            bool trully = true;
+            int i, j;
+            i = 0;
+            j = 0;
+            while (trully)
+            {
+                string? decision = Console.ReadLine();
+                if (decision == "Yes")
+                {
+                    yesOrNo.Add(decision+ " " + i);
+                    i++;
+                }
+                else if (decision == "No")
+                {
+                    yesOrNo.Add(decision + " " + j);
+                    j++;
+                }
+                else if(decision == null)
+                {
+                    Console.WriteLine("You didn't wont");
+                }
+                else if(decision == "0")
+                {
+                    trully = false;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Vote is incorrect");
+                }
+            }
+            foreach (var voting in yesOrNo)
+            {
+                Console.WriteLine(voting);
+            }
+
+        }
+    }
+}
